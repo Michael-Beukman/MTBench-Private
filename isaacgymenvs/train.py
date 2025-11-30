61a17d5c39ae8873b87a4ea2276bf552fb116ccf
Michael Beukman <michael.beukman@gmail.com>
2025-11-30 22:08:17 +0000

    nuke wandb

--- isaacgymenvs/train.py
+++ isaacgymenvs/train.py
@@ -38,6 +38,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import logging
 import os
 from datetime import datetime
+os.environ["WANDB_DISABLE_SERVICE"] = "True"
 
 # noinspection PyUnresolvedReferences
 import isaacgym
